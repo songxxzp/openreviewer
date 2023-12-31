e6212f7b5a634666a5baf4686a6e144a49c2f450
Song XiXuan <songxxzp@163.com>
2023-12-31 11:24:44 +0800

    Update data_processor

--- data_processor.py
+++ data_processor.py
@@ -4,14 +4,79 @@ import os
 from typing import List, Dict, Any
 
 
-pdfs_path = "/root/autodl-tmp/data/1204/pdfstxt"
+# pdfs_path = "/root/autodl-tmp/data/1204/pdfstxt"
+pdfs_path = "/root/autodl-tmp/data/iclr2024/pdfstxt"
 note_path = "/root/autodl-tmp/workspace/openreviewer/data/iclr2024/notes.jsonl"
 reviews_path = "/root/autodl-tmp/workspace/openreviewer/data/iclr2024/reviews"
+splited_reviews_path = "/root/autodl-tmp/workspace/openreviewer/data/iclr2024/reviews_2"
 rebuttals_path = "/root/autodl-tmp/workspace/openreviewer/data/iclr2024/rebuttals"
-save_path = "/root/autodl-tmp/data/iclr2024/1204.jsonl"
+# save_path = "/root/autodl-tmp/data/iclr2024/1227.jsonl"
+save_path = "/root/autodl-tmp/workspace/openreviewer/example-1227.jsonl"
+
+# pdfs_path = "data/iclr2024/pdfstxt"
+# note_path = "data/iclr2024/notes.jsonl"
+# reviews_path = "data/iclr2024/reviews"
+# rebuttals_path = "data/iclr2024/rebuttals"
+# save_path = "data/iclr2024/1204_3k.jsonl"
+
+chapters = {
+    "ABSTRACT": True,
+    "INTRODUCTION": True,
+    "RELATED WORK": False,
+    "EXPERIMENTS": True,
+    "RESULTS": True,
+    "DISCUSSION": False, 
+    "CONCLUSION": True, 
+    "REFERENCES": False
+}
 
 
-def review_parser(review: Dict, forum: str) -> Dict[str, Any]:
+def split_iclr_pdf(content, keep_chapters) -> Dict[str, str]:
+    paper_dict = {}
+    keys = list(keep_chapters.keys())
+    for i in range(len(keep_chapters.keys())):
+        if(not keep_chapters[keys[i]]):
+            continue
+        start_key = keys[i]
+        end_key = keys[i + 1] if i + 1 < len(keys) else None
+        paper_dict[start_key] = extract_part(content, start_key, end_key)
+
+    return paper_dict
+
+
+def filter_iclr_pdf(content, keep_chapters) -> str:
+    # iclr键值
+
+    paper_dict = {}
+    keys = list(keep_chapters.keys())
+    for i in range(len(keep_chapters.keys())):
+        if(not keep_chapters[keys[i]]):
+            continue
+        start_key = keys[i]
+        end_key = keys[i + 1] if i + 1 < len(keys) else None
+        paper_dict[start_key] = extract_part(content, start_key, end_key)
+
+    formatted_string = ""
+    for key, value in paper_dict.items():
+        formatted_string += f"{key}\n\n{value}\n\n\n"
+
+    return formatted_string
+
+
+def extract_part(text, start_key, end_key) -> str:
+    start_index = text.lower().find(start_key.lower())
+    if start_index == -1:
+        return ""
+    
+    if end_key:
+        end_index = text.lower().find(end_key.lower(), start_index)
+    else:
+        end_index = len(text)
+
+    return text[start_index:end_index].strip()
+
+
+def review_parser(review: Dict, splited_review: Dict, forum: str) -> Dict[str, Any]:
     assert review["replyto"] == forum
     assert review["forum"] == forum
 
@@ -28,6 +93,10 @@ def review_parser(review: Dict, forum: str) -> Dict[str, Any]:
         "strengths": review["content"]["strengths"]["value"],
         "weaknesses": review["content"]["weaknesses"]["value"],
         "questions": review["content"]["questions"]["value"],
+        "splited_summaries": splited_review["summary"],
+        "splited_questions": splited_review["questions"],
+        "splited_weaknesses": splited_review["weaknesses"],
+        "splited_strengths": splited_review["strengths"],
     }
 
     parsed_review['score'] = score_parser(parsed_review)
@@ -35,8 +104,8 @@ def review_parser(review: Dict, forum: str) -> Dict[str, Any]:
     return parsed_review
 
 
-def reviews_parser(reviews: List, forum: str) -> List:
-    return [review_parser(review, forum) for review in reviews]
+def reviews_parser(reviews: List, splited_reviews: List, forum: str) -> List:
+    return [review_parser(review, splited_review["Reviews"], forum) for (review, splited_review) in zip(reviews, splited_reviews)]
 
 
 def score_parser(parsed_review: Dict[str, Any]) -> Dict[str, int]:
@@ -53,14 +122,27 @@ def score_parser(parsed_review: Dict[str, Any]) -> Dict[str, int]:
 def main():
     with open(note_path, "r", encoding="utf-8") as f:
         notes = [json.loads(l) for l in f]
+    
+    empty = 0
     pdfs = {}
     for pdf_path in os.listdir(pdfs_path):
         with open(os.path.join(pdfs_path, pdf_path), "r", encoding="utf-8") as f:
-            pdfs[pdf_path.replace(".txt", '')] = f.read()
+            content = f.read()
+            if content:
+                pdfs[pdf_path.replace(".txt", '')] = content
+            else:
+                empty += 1
+                print(pdf_path)
+    print("Empty pdfs:", empty)
+
     reviews = {}
     for review_path in os.listdir(reviews_path):
         with open(os.path.join(reviews_path, review_path), "r", encoding="utf-8") as f:
             reviews[review_path.replace(".json", '')] = json.load(f)
+    splited_reviews = {}
+    for splited_review_path in os.listdir(splited_reviews_path):
+        with open(os.path.join(splited_reviews_path, splited_review_path), "r", encoding="utf-8") as f:
+            splited_reviews[splited_review_path.replace(".json", '')] = json.load(f)
     rebuttals = {}
     if os.path.exists(rebuttals_path):
         for rebuttal_path in os.listdir(rebuttals_path):
@@ -76,13 +158,17 @@ def main():
 
     for note in notes:
         assert note["id"] == note["forum"]
+        if (not note["forum"] in pdfs) or (not note["forum"] in reviews) or (not note["forum"] in splited_reviews):
+            print(note["forum"])
+            continue
         sample = {
             "Title": note["content"]["title"]["value"],
             "Author": None,
             "Abstract": note["content"]["abstract"]["value"],
             "Keywords": note["content"]["keywords"]["value"],
-            "Reviews": reviews_parser(reviews[note["forum"]], note["forum"]) if note["forum"] in reviews else None,
+            "Reviews": reviews_parser(reviews[note["forum"]], splited_reviews[note["forum"]], note["forum"]) if note["forum"] in reviews else None,
             "Text": pdfs[note["forum"]] if note["forum"] in pdfs else None,
+            "Splited_Text": split_iclr_pdf(pdfs[note["forum"]], chapters) if note["forum"] in pdfs else None,
             # "Rebuttals": rebuttals[note["forum"]] if note["forum"] in rebuttals else None,
             # "Scores": "List[int](same number to Reviews)",
             # "References": "List[str]",
@@ -93,6 +179,7 @@ def main():
             "forum": note["forum"],
         }
         samples.append(sample)
+        break
     
     os.makedirs(os.path.dirname(save_path), exist_ok=True)
     with open(save_path, 'w', encoding='utf-8') as f:
